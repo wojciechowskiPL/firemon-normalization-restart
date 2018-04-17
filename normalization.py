f506f6233195ef24b5d78ed049e9eb90cf6e120c
devu <devurien@gmail.com>
2018-04-17 16:24:16 +0200

    Initial commit

---
+++ normalization.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import argparse
+import requests
+from requests.exceptions import RequestException
+
+
+def requestor(result):
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+
+    host = result.Host
+    start = result.start
+    end = result.end
+    login = result.login
+    password = result.password
+
+    for i in range(start, end):
+        # Sample CURL
+        ## curl curl -k -X PUT -u 'firemon:firemon' S--header 'Content-Type: application/json' --header 'Accept: application/json' 'https://172.31.137.173/securitymanager/api/rev/1/nd/restart'
+        try:
+            requests.put('https://{}/securitymanager/api/rev/{}/nd/restart'.format(host, i),
+                                headers=headers,
+                                verify=False,
+                                auth=(login, password))
+        except RequestException as e:
+            print('Connection error: {}'.format(e))
+            sys.exit(1)
+
+    # Sample CURL
+    ## curl curl -k -X PUT -u 'firemon:firemon' --header 'Content-Type: application/json' --header 'Accept: application/json' 'https://172.31.137.173/securitymanager/api/rev/interrupted/requeue'
+    try:
+        requests.put('https://{}/securitymanager/api/rev/interrupted/requeue'.format(host),
+                            headers=headers,
+                            verify=False,
+                            auth=(login, password))
+    except RequestException as e:
+        print('Connection error: {}'.format(e))
+        sys.exit(1)
+
+
+def args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-H", "--Host", type=str,
+                        help="Host",
+                        required=True)
+    parser.add_argument("-l", "--login", type=str,
+                        help="login",
+                        required=True)
+    parser.add_argument("-p", "--password", type=str,
+                        help="password",
+                        required=True)
+    parser.add_argument("-s", "--start", type=int,
+                        help="start",
+                        required=True)
+    parser.add_argument("-e", "--end", type=int,
+                        help="end",
+                        required=True)
+
+    result = parser.parse_args()
+
+    return result
+
+
+if __name__ == "__main__":
+
+    requestor(args())
